53b44447597c75f5d4618902618f525c5cfdc0fa
BenGirard98 <ben.girard98@gmail.com>
2024-12-12 12:51:14 -0600

    Edited Trade typing's datafield to accomadate any type data

--- src/types/Trade.ts
+++ src/types/Trade.ts
@@ -4,5 +4,5 @@ export interface Trade {
   cardDescription: string; // Short description for the trade card
   modalDescription: string; // Longer more in depth desciption to be displayed when clicked
   onClick: () => void; // Function to handle clicks
-  data: [];
+  data: any;
 }
